abecaa88cc62b53b76ca925f492fd66583333132
wlane <will@asklane.com>
2016-05-04 21:19:17 -0700

    reverted back to simple NER example

--- ner_tagger.py
+++ ner_tagger.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 stanford_tagger = StanfordNERTagger(
     'Resources/NER_Models/english.muc.7class.nodistsim.crf.ser.gz',
-    'Resources/StanfordNER/stanford-ner.jar',
+    '/home/wlane/stanford-ner-2015-04-20/stanford-ner.jar',
     encoding='utf-8')
 
 text = 'While surfing in Morocco on June 24th, Jane figured out that Danny DeVito was pretending to be a shark and ' \
@@ -21,13 +21,13 @@ print(classified_text)
 ## TRAIN our own NER model : JaneAusten "Emma" Tutorial #
 #########################################################
 
-# Tokenize the training text
-with open("Resources/emmaCh1.txt") as f:
-    data=f.read().replace('\n', ' ')
-tokenized_emma = word_tokenize(data)
+# # Tokenize the training text
+# with open("Resources/emmaCh1.txt") as f:
+#     data=f.read().replace('\n', ' ')
+# tokenized_emma = word_tokenize(data)
 
 # Write output in format suitable for NER training input
-emma_train_file = open("Resources/emma_train.tsv", "w")
-for tok in tokenized_emma:
-    emma_train_file.write(tok + "\tO\n" ) # Ideally we'd mark entities as well
-emma_train_file.close()
+# emma_train_file = open("Resources/emma_train.tsv", "w")
+# for tok in tokenized_emma:
+#     emma_train_file.write(tok + "\tO\n" ) # Ideally we'd mark entities as well
+# emma_train_file.close()
